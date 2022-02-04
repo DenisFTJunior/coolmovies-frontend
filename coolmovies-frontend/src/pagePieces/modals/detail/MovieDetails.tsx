add68d9917d1034d4fd1cae953e1063c3283ccd0
Denis Junior <junior.denis2354@outlook.com>
2022-02-03 22:07:45 -0300

    DetailMovieModal add

--- coolmovies-frontend/src/pagePieces/modals/detail/MovieDetails.tsx
+++ coolmovies-frontend/src/pagePieces/modals/detail/MovieDetails.tsx
@@ -1,18 +1,27 @@
-import { ShowDirectorById } from "../../../components/show/ShowDirector";
-import { ShowReviewById } from "../../../components/show/ShowReview";
+import React from "react";
+import DetailsModal from "../../../components/modal/DetailsModal";
+
+import { ShowDirector } from "../../../components/show/ShowDirector";
+import { ShowReview } from "../../../components/show/ShowReview";
 import { DetailItem } from "../../../schema/components/Modal";
 
-const detailModalItems: DetailItem[] = [
+const ITEMS: DetailItem[] = [
   { label: "Title", prop: "title" },
   { label: "Release Date", prop: "releaseDate" },
   {
     prop: "directorId",
     label: "Director",
-    render: (data, item) => <ShowDirectorById id={data.movieDirectorId} />,
+    render: (data, item) => <ShowDirector director={data.director} />,
   },
   {
     prop: "reviewId",
     label: "Review",
-    render: (data, item) => <ShowReviewById movieId={data.id} />,
+    render: (data, item) => <ShowReview review={data.review} />,
   },
 ];
+
+const DetailMovieModal = () =>{
+  <DetailsModal name="detailMovie" items={ITEMS}/> 
+}
+
+export default DetailMovieModal
